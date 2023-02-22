f2f285eb5ff79da520e4a6907b5221c395dcdab7
zach-laptop <zach.macdonald.021@gmail.com>
2023-02-22 18:27:48 -0500

    oops

--- composables/useCalendarApi.ts
+++ composables/useCalendarApi.ts
@@ -6,7 +6,6 @@ const selectedCalendarId = useSessionStorage('selectedCalendarId', '')
 export default function (provider_token: string) {
 	const ax = axios.create({
 		headers: {
-			// Authorization: `Bearer ya29.a0AVvZVsrCw8__0H_GU4tAczmi31LjX233DenIOg2-9KFEX5_cs5QQLLoEmDRmNQMh3QcoXt8whC0dJndvndJ6cNhvcVULKJiyh2bYP2y0Ys2Rps2tJvi1N5NA8cdMdUKulW2Ef1axm_i5z-MjH4mfGlkBfJm1ZdIvRvndaCgYKAXkSARISFQGbdwaIld0ywhcOe97K4pdxGi2SGA0171`
 			Authorization: `Bearer ${provider_token}`
 		}
 	})
